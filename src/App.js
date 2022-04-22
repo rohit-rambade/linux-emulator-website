74a0911946b6e99687be3d78305db5513205734c
rohit-rambade <rohitrambade18@gmail.com>
2022-04-22 10:35:30 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { BrowserRouter as Router,  Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar } from "./Components/Navbar/Navbar";
-import Body from "./Components/SideNav/Body";
+import IntrotoLinux from "./Components/SideNav/IntrotoLinux";
+import LinuxHistory from "./Components/SideNav/LinuxHistory";
+
 import { About } from "./pages/About/About";
 import { Home } from "./pages/Home/Home";
 import { Terminal } from "./pages/Terminal/Terminal";
@@ -11,19 +13,20 @@ const App = () => {
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
+
         <Routes>
-          <Route path='/' exact element={<Home/>} />
-          <Route path='/terminal' element={<Terminal/>} />
-          <Route path='/about' element={<About/>} />
-          
+          <Route path="/" exact element={<Home />}>
+            <Route path="linuxhistory" element={<LinuxHistory />} />
+
+            <Route path="linuxintro" element={<IntrotoLinux />} />
+          </Route>
+
+          <Route path="/terminal" element={<Terminal />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
-      
     </>
-   
-   
-   
   );
 };
 
