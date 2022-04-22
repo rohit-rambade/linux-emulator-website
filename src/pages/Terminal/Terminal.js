3d6be4f67a218911c5b11be6f2404feb8d8bf550
rohit-rambade <rohitrambade18@gmail.com>
2022-04-22 20:46:57 +0530

    Added Terminal

--- src/pages/Terminal/Terminal.js
+++ src/pages/Terminal/Terminal.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Terminal.css";
 
 export const Terminal = () => {
+  function docker_cmd() {
+    var i = document.getElementById("in1").value;
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "http://<enter_your_ip>/cgi-bin/docker.py?x=" + i, true);
+
+    xhr.send();
+
+    xhr.onload = function () {
+      var output = xhr.responseText;
+      document.getElementById("d1").innerHTML = output;
+    };
+  }
+
   return (
-    <div><h1>this is terminal</h1></div>
-  )
-}
+    <div>
+      <div id="terminal">
+        <div id="terminal_head">
+          <i>root@localhost:DockerWebsite</i>
+        </div>
+
+        <div id="b">
+          <div>
+            [root@localhost~]#{" "}
+            <input id="in1" placeholder="Enter your command here" />
+          </div>
+          <pre>
+            <div id="d1"> </div>
+          </pre>
+        </div>
+      </div>
+
+      <div id="b1">
+        <button onclick={docker_cmd()} id="submit_btn">
+          Click
+        </button>
+      </div>
+    </div>
+  );
+};
