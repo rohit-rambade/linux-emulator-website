7bbac756e31f47c405b30bb27c0cff6fefef3c4f
rohit-rambade <rohitrambade18@gmail.com>
2022-05-01 21:13:07 +0530

    some changes by paresh

--- src/pages/Terminal/Terminal_input.jsx
+++ src/pages/Terminal/Terminal_input.jsx
@@ -3,44 +3,62 @@ import "./Terminal.css"
 
 function Terminal_input() {
 
+  const [output , setOutput]=useState([])
+  const [cmdInput, setCmdInput] = useState("")
+  const handleChange = (event) =>{
+    setCmdInput(event.target.value)
+  }
     
   function docker_cmd(){
-
     console.log("clicked")
     console.log(document.getElementById("in1").value)
     
     var i = document.getElementById("in1").value
     var xhr = new XMLHttpRequest();
-    xhr.open("GET" , "http://3.110.43.108/cgi-bin/docker.py?x="+i,true);
+    xhr.open("GET" , "http://65.2.150.152/cgi-bin/docker.py?x="+i,true);
     xhr.send()
     xhr.onload = function (){
       const output1 = xhr.responseText;
-      console.log(output1)
-      setOutput(output1)
+      let obj = {
+        "cmd" : "[root@localhost~]# "+ cmdInput,
+        "output" : output1
+      }
+      let opArray = output;
+      opArray.push(obj)
+      console.log(xhr.response);
+      setOutput(opArray)
+      console.log(output);
       //   document.getElementById("d1").innerHTML = output;
     }
     
     }
     
-    const [output , setOutput]=useState(" ")
   
-
-
-
-
 
   return (
     <div id="terminal_block">
-			  <div>
-				  [root@localhost~]# <input id = "in1" placeholder="Enter your command here" />
-			  </div>
 			  <pre>
-				  <div id = "d1">{output}</div> 
+          {
+            // output.lenght !== 0 && output !== null?
+            output.map( (obj, i) =>(
+              // <div key={i} className="cmdContainer">
+              <>
+                <div id = "d1" >{obj.output}</div>
+                <div className='cmd'>{obj.cmd}</div>
+              </>
+              // </div>
+            )) 
+            // : <div className="di"></div>
+          }
 			  </pre>
-
-              <div id="button_div">
-          <button  onClick={docker_cmd} id="submit_btn">Click</button>
-    </div>
+        {/* <form> */}
+          <div>
+            [root@localhost~]# <input id = "in1" placeholder="Enter your command here" value={cmdInput} onChange={handleChange}/>
+          </div>
+          <div id="button_div">
+            <button  onClick={() => docker_cmd()} id="submit_btn">Click</button>
+          </div>
+        {/* </form> */}
     </div>
   )
 }
