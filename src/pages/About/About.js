8dc6270dada9ca09718c0dfb316a1aec621d41de
rohit-rambade <rohitrambade18@gmail.com>
2022-05-04 12:33:13 +0530

    error

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -21,6 +21,66 @@ export const About = () => {
             practicing Linux concepts. Along with this there is chatbot
             which gives you easy to help support facility.
         </p>
+      
     </div>
+    
+       
   )
 }
+
+  // <section id="skills">
+
+  //       <div className="skills">
+  //           <div className="container">
+
+  //               <div className="skills-title">
+
+  //                   <h2>Skills</h2>
+  //               </div>
+
+
+  //               <div className="skills-list">
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-python-plain colored"></i></div><label
+  //                           className="skill-name">Python</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-c-plain colored"></i></div><label
+  //                           className="skill-name">C</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-linux-plain colored"></i></div><label
+  //                           className="skill-name">Linux</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-github-original colored"></i></div><label
+  //                           className="skill-name">GitHub</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-docker-plain colored"></i></div><label
+  //                           className="skill-name">Docker</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-kubernetes-plain colored"></i></div><label
+  //                           className="skill-name">Kubernets</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-googlecloud-plain colored"></i></div><label
+  //                           className="skill-name">GoogleCloud</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-html5-plain colored"></i></div><label
+  //                           className="skill-name">HTML</label>
+  //                   </div>
+  //                   <div className="skill-card">
+  //                       <div className="skill-icon"><i className="devicon-css3-plain colored"></i></div><label
+  //                           className="skill-name">CSS</label>
+  //                   </div>
+  //               </div>
+
+
+
+  //           </div>
+  //       </div>
+
+  //   </section>
