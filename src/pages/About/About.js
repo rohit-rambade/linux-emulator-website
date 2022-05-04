de1ff7f03bdc98084fc189d23e48358a16a9a89e
rohit-rambade <rohitrambade18@gmail.com>
2022-05-04 12:27:23 +0530

    added about info

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,7 +1,26 @@
 import React from 'react'
-
+import "./About.css"
 export const About = () => {
   return (
-    <div><h1>this i  about</h1></div>
+    
+    <div className='abt'>
+        <h1>About</h1>
+        <p>
+            In this world of technology data and security is biggest
+            concern and Linux is considered as one of most secured
+            operating system around the world. Having knowledge of Linux
+            is always great thing. But learning Linux might be difficult
+            sometimes .To solve this issue here's our project Linux
+            emulator.
+        </p>
+        <p>
+            "Linux Emulator web application" project is web based
+            application which can provide service to learn , practice and
+            test Linux commands. In this project you will come to know
+            Linux concepts and Linux environment in user friendly way for
+            practicing Linux concepts. Along with this there is chatbot
+            which gives you easy to help support facility.
+        </p>
+    </div>
   )
 }
